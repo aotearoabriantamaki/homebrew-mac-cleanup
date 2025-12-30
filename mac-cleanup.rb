f869d84ccbb826d93fab7a3d3382b9e216d68c67
aotearoabriantamaki <brian.tamaki@outlook.com>
2025-12-30 17:01:11 +1300

    Update mac-cleanup.rb

--- mac-cleanup.rb
+++ mac-cleanup.rb
@@ -1,9 +1,9 @@
-class MacCleanup < Formula
+ class MacCleanup < Formula
     desc "Interactive macOS system cleanup tool"
     homepage "https://github.com/aotearoabriantamaki/mac-cleanup"
     url "https://raw.githubusercontent.com/aotearoabriantamaki/mac-cleanup/main/mac-cleanup"
     version "1.0.0"
-    sha256 :"89272f02e81f8c28a5d884827e4c4967d04c529e9c54c9d5ab86b1a38b61966e"
+    sha256 "89272f02e81f8c28a5d884827e4c4967d04c529e9c54c9d5ab86b1a38b61966e"
     license "MIT"
 
     def install
